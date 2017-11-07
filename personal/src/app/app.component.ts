fa33b6f551ad2f5971eb91589f0448e6adb3d158
Emil Sunesson <sunesson.emil@gmail.com>
2017-11-07 21:41:38 +0100

    mostly animations

--- personal/src/app/app.component.ts
+++ personal/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  constructor(private router: Router) {
+  }
+
+  public isRoute(route: string) {
+    return this.router.url == route;
+  }
 }
--- personal/src/app/home.component.ts
+++ personal/src/app/home.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  @ViewChild('cmd') cmd: ElementRef;
+
+  private index = 0;
+  private text = "testing the typewriter";
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    let subscription = Observable.interval(100).subscribe(it => {
+      if(this.index == this.text.length) {
+        subscription.unsubscribe();
+      } else {
+        this.write();
+      }
+    });
+  }
+
+  private write() {
+    this.cmd.nativeElement.innerText += this.text[this.index];
+    this.index++;
+  }
+
 }
