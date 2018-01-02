1fe6328d54ab2d662f79802646ec8d2155dbbfae
Emil Sunesson <sunesson.emil@gmail.com>
2018-01-03 00:03:16 +0100

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor(private router: Router) {
-  }
-
-  public isRoute(route: string) {
-    return this.router.url == route;
+  constructor() {
   }
 }
