6d0800f899ed9e2bbcc72e49f1b100c14f7d4aae
Emil Sunesson <sunesson.emil@gmail.com>
2020-05-23 11:36:02 +0200

    Updated doks

--- src/app/code/ngx-tree/ngx-tree-api/ngx-tree-api.component.ts
+++ src/app/code/ngx-tree/ngx-tree-api/ngx-tree-api.component.ts
@@ -5,20 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './ngx-tree-api.component.html'
 })
 export class NgxTreeApiComponent {
-
-  public nodeItemType = 'NodeItem<T>';
-  public selectedItemsType = 'EventEmitter<any>';
-  public addByIdSignature = 'addNodeById(nodeItem: NodeItem<any>, id: string)';
-  public deleteByIdSignature = 'deleteById(id: string)';
-  public editNameByIdSignature = 'editNameById(id: string, name: string)';
-  public editItemByIdSignature = 'editItemById(id: string, item: any)';
-  public childrenSignature = 'children?: NodeItem<any>[]';
-  public nameCallbackSignature = 'nameClick?: (item: NodeItem<any>) => void';
-  public selectCallbackSignature = 'select?: (item: NodeItem<any>) => void';
-  public unSelectCallbackSignature = 'unSelect?: (item: NodeItem<any>) => void';
-  public toggleCallbackSignature = 'toggle?: (item: NodeItem<any>) => void';
-  
-
-  constructor() {
-  }
 }
